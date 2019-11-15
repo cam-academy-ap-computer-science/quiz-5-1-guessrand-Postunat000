0928665b0c947337b3743a0d5ca1da60b87ef706
postunat000 <postunat000@CAM-LAB-10.bgsd.k12.wa.us>
2019-11-15 07:29:51 -0800

    iThinkIActuallyDidIt.java

--- src/GuessRand.java
+++ src/GuessRand.java
@@ -1,33 +1,35 @@
-import java.util.*;   
+import java.util.*;
+
 public class GuessRand {
 
 	public static void main(String[] args) {
-		int rand = 0;
-		boolean success = false;
-		// Instantiate (create) a new Random() object 'r'
-		rand = // Get a random number between 1-100
+		Scanner console = new Scanner(System.in); // Instantiate a new Scanner 'console'
+		Random r = new Random(); // Instantiate (create) a new Random() object 'r'
 		
-		while (    // loop until 'success' is true. ) {
-			int guess = 0;
-			// Instantiate a new Scanner 'console'
-			System.out.println("Your Guess: ?");
-			guess = // Get an integer from the user.
-			success =  // call 'compare()' method
+		int guess = 0;
+		int rand = 0;
+		
+		boolean success = false;
+		rand = r.nextInt(101) + 0; // Get a random number between 1-100
+		while (success == false) { // loop until 'success' is true. 
+			System.out.println("Your Guess: ? ");
+			guess = console.nextInt(); // Get an integer from the user.
+			success =  compare(guess, rand); // call 'compare()' method
 		}
 	}
 	
 	// Declare a method named 'compare()' that accepts two parameters 'guess' and 'rand' and returns
 	// a boolean.
-	public static   
+	public static boolean compare(int guess, int rand) {
 		boolean got_it = false;
-		if () {  // Fix if test
+		if (guess == rand) {  // Fix if test
 			System.out.println("You got it!");
 			got_it = true;
-		} else if () {  // Fix if test
+		} else if (guess <= rand) {  // Fix if test
 			System.out.println("No, it's Higher");
 		} else {
 			System.out.println("No, it's Lower");
 		}
-		return got_it;
+		return (boolean) got_it;
 	}
 }
